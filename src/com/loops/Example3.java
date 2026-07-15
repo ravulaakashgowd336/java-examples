7ed7ff85a88396a7e0e5148015291c8e0246569a
Akash <akashravula7@gmail.com>
2026-07-15 14:55:40 +0530

    hi

--- src/com/loops/Example3.java
+++ src/com/loops/Example3.java
@@ -2,38 +2,33 @@ package com.loops;
 
 public class Example3 {
 
-    public boolean isUglyNumber(int num) {
+    public boolean isHarshadNumber(int num) {
 
         if (num <= 0) {
             return false;
         }
 
         int temp = num;
+        int sum = 0;
 
-        while (temp % 2 == 0) {
-            temp = temp / 2;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
         }
 
-        while (temp % 3 == 0) {
-            temp = temp / 3;
-        }
-
-        while (temp % 5 == 0) {
-            temp = temp / 5;
-        }
-
-        return temp == 1;
+        return num % sum == 0;
     }
 
     public static void main(String[] args) {
 
         Example3 obj = new Example3();
 
-        int num = 6;
+        int num = 18;
 
-        System.out.println("Enter the number: " + num);
-        System.out.println(obj.isUglyNumber(num)
-                ? "Is Ugly Number"
-                : "Is Not Ugly Number");
+        System.out.println("Number: " + num);
+        System.out.println(obj.isHarshadNumber(num)
+                ? "Harshad Number"
+                : "Not a Harshad Number");
     }
 }
+   
