cb3080d722bcba1b786c14b09b9e866a36bb707a
Akash <akashravula7@gmail.com>
2026-07-15 14:49:34 +0530

    hi

---
+++ src/com/loops/Example3.java
@@ -0,0 +1,39 @@
+package com.loops;
+
+public class Example3 {
+
+    public boolean isUglyNumber(int num) {
+
+        if (num <= 0) {
+            return false;
+        }
+
+        int temp = num;
+
+        while (temp % 2 == 0) {
+            temp = temp / 2;
+        }
+
+        while (temp % 3 == 0) {
+            temp = temp / 3;
+        }
+
+        while (temp % 5 == 0) {
+            temp = temp / 5;
+        }
+
+        return temp == 1;
+    }
+
+    public static void main(String[] args) {
+
+        Example3 obj = new Example3();
+
+        int num = 6;
+
+        System.out.println("Enter the number: " + num);
+        System.out.println(obj.isUglyNumber(num)
+                ? "Is Ugly Number"
+                : "Is Not Ugly Number");
+    }
+}
