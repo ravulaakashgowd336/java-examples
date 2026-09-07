26426e0f5d0aba04c93403e53a81b33e7fcfea11
Akash <akashravula7@gmail.com>
2026-09-07 21:07:39 +0530

    Temperature

---
+++ src/temperatures/CelsiusToFahrenheit.java
@@ -0,0 +1,12 @@
+package temperatures;
+import java.util.Scanner;
+public class CelsiusToFahrenheit {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Celsius: ");
+		double c = sc.nextDouble();
+		double f = (c*9/5)+32;
+		System.out.println("Fahrenheit = " + f);
+		sc.close();
+	}
+}
